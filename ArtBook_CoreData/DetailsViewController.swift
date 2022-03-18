f96ba6f797b0d8e54cf98d8e26fc6a4decd64736
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-18 12:26:34 +0300

    config kb delegate

--- ArtBook_CoreData/DetailsViewController.swift
+++ ArtBook_CoreData/DetailsViewController.swift
@@ -12,12 +12,53 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var authorTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var topCnsrtntMainStack: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        config()
     }
     
+    private func config() {
+        configTapGestureRecognizer()
+        configTextField()
+    }
+    
+    private func configTapGestureRecognizer() {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(recognizer)
+    }
+    
+    @objc private func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    private func configTextField() {
+        nameTextField.delegate = self
+        authorTextField.delegate = self
+        yearTextField.delegate = self
+    }
+    
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
+extension DetailsViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        topCnsrtntMainStack.constant = -200
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        topCnsrtntMainStack.constant = 40
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
--- ArtBook_CoreData/ViewController.swift
+++ ArtBook_CoreData/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        config()
+    }
+    
+    private func config() {
+        configTabBar()
+    }
+    
+    private func configTabBar() {
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+    }
+    
+    @objc private func addButtonClicked() {
+        performSegue(withIdentifier: Constants.segueID, sender: nil)
     }
 }
 
