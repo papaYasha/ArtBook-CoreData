fbbbe2b2602d1ea507d9d12dca8cac25fa8f75be
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-18 09:55:42 +0300

    Init project

---
+++ ArtBook_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook_CoreData
+//
+//  Created by Macbook on 18.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
