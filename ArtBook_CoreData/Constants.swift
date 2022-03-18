ef1d89524479910bdecf5b0bb1021d1785312d64
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-18 10:28:16 +0300

    Make design and connection

---
+++ ArtBook_CoreData/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  ArtBook_CoreData
+//
+//  Created by Macbook on 18.03.22.
+//
+
+import Foundation
+
+struct Constants {
+    static let segueID = "2Details"
+}
---
+++ ArtBook_CoreData/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  ArtBook_CoreData
+//
+//  Created by Macbook on 18.03.22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
+}
